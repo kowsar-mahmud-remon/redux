1e65e0073c9fdccaab44db8d85a6638727f5584c
creativerze <creativerze@gmail.com>
2023-01-07 13:40:55 +0600

    added increment or decrement

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const { createStore } = require('redux');
+
 // defining constants
 const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
-const ADD_USER = 'ADD_USER';
 
 // state
 const initialCounterState = {
@@ -29,10 +30,39 @@ const decrementCounter = () => {
   };
 };
 
-const addUser = () => {
-  return {
-    type: ADD_USER,
-    payload: { name: 'remon' }
-  };
-}
+// create reducer for counter
+const counterReducer = (state = initialCounterState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1
+      };
 
+    default:
+      state;
+  }
+};
+
+// 1. state
+// 2. dispatch action
+// 3. reducer
+// 4. store - getState(), despatch(), subscribe()
+
+// create store
+const store = createStore(counterReducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+// dispatch action
+store.dispatch(incrementCounter());
+store.dispatch(incrementCounter());
+store.dispatch(incrementCounter());
+store.dispatch(decrementCounter());
